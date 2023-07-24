8a7d7bf44ecb7d3b8c67414a744b495a193e870d
Vitaliy Nijankovskiy <vvitto123@gmail.com>
2023-07-24 16:14:32 +0300

    fixed typo#2

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -139,7 +139,6 @@ class SIPUAHelper extends EventManager {
     _settings.dtmf_mode = uaSettings.dtmfMode;
     _settings.session_timers = uaSettings.sessionTimers;
     _settings.ice_gathering_timeout = uaSettings.iceGatheringTimeout;
-    _settings.instance_id = uiSettings.instanceId;
     _settings.session_timers_refresh_method =
         uaSettings.sessionTimersRefreshMethod;
     _settings.instance_id = uaSettings.instanceId;
