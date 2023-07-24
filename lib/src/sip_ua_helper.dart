8f5618213932e68ddac554de6ae8b21cc33e1ca7
Vitaliy Nijankovskiy <vvitto123@gmail.com>
2023-07-24 15:47:12 +0300

    Added instance_id to SipUaHelper

--- lib/src/sip_ua_helper.dart
+++ lib/src/sip_ua_helper.dart
@@ -139,6 +139,7 @@ class SIPUAHelper extends EventManager {
     _settings.dtmf_mode = uaSettings.dtmfMode;
     _settings.session_timers = uaSettings.sessionTimers;
     _settings.ice_gathering_timeout = uaSettings.iceGatheringTimeout;
+    _settings.instance_id = uiSettings.instanceId;
 
     try {
       _ua = UA(_settings);
@@ -705,6 +706,7 @@ class UaSettings {
   String? password;
   String? ha1;
   String? displayName;
+  String? instanceId;
 
   /// DTMF mode, in band (rfc2833) or out of band (sip info)
   DtmfMode dtmfMode = DtmfMode.INFO;
